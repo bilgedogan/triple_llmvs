15c6e2d96b61c256de4cdc4d84d8111de72e2819
Nadide Bilge Dogan <doganbilge.bd@gmail.com>
2026-05-17 23:16:41 +0300

    v2.0.0
    *****************************************************
    * Author: Nadide Bilge Dogan                        *
    * Date: 18/05/2026                                  *
    * Version: 2.0.0                                    *
    * Branch: main                                      *
    * Development:                                      *
    * - gitignore updated.                              *
    *****************************************************

--- version.py
+++ version.py
@@ -1,6 +1,16 @@
 """
 *****************************************************
 * Author: Nadide Bilge Dogan                        *
+* Date: 18/05/2026                                  *
+* Version: 2.0.0                                    *
+* Branch: main                                      *
+* Development:                                      *
+* - gitignore updated.                              *
+*****************************************************
+"""
+"""
+*****************************************************
+* Author: Nadide Bilge Dogan                        *
 * Date: 30/04/2026                                  *
 * Version: 1.0.0                                    *
 * Branch: main                                      *
